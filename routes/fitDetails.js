b9b3a596b00a541c599a25cc1bc12ee7df61ef80
arupsarkar <sarkar.arup@gmail.com>
2020-05-28 23:59:58 -0400

    update of fit info

--- routes/fitDetails.js
+++ routes/fitDetails.js
@@ -5,7 +5,7 @@ const client = require('../db');
 
 router.get('/', async (req, res, next) => {
     try{
-        let sql = 'select Size__c, Type__c, Contact__c from salesforce.bnb_fit_info__c';
+        let sql = 'select Size__c, Type__c, Id from salesforce.bnb_fit_info__c';
 
         await client
             .query(sql)
@@ -18,6 +18,27 @@ router.get('/', async (req, res, next) => {
         console.error(err.message);
     }
 });
+
+router.put('/update', async(req, res, next) => {
+    try{
+        const sql = 'update salesforce.bnb_fit_info__c SET size__c = $1, type__c = $2 WHERE id = $3';
+        const {size__c} = req.body.size;
+        const {type__c} = req.body.type;
+        const {id} = req.body.id;
+        console.log(new Date(), ' size : ' + size__c);
+        console.log(new Date(), ' type : ' + type__c);
+        console.log(new Date(), ' id : ' + id);
+        await client
+            .query(sql, [size__c, type__c, id ])
+            .then(result => {
+                console.log(new Date(), JSON.stringify(result));
+                res.json({result: 'Record updated successfully.'});
+            })
+            .catch(err => console.error('Error executing query', err.stack));
+    }catch(err) {
+        console.error(err.message);
+    }
+})
 
 
 module.exports = router;

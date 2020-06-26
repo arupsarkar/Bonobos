b9fee349364077a84c7faba23803f9e889b9f99d
arupsarkar-sfdc <arup.sarkar@salesforce.com>
2020-06-25 23:27:43 -0400

    added db streaming

--- db.js
+++ db.js
@@ -20,5 +20,18 @@ let client = new pg.Client({
 });
 
 client.connect();
+let db_url = 'postgres://qvkjbbfwzcfvut:a19763df01d1eb026c84e6d780a5b34f23d5dbecce238f53e90b41f8e0f331d8@ec2-18-210-51-239.compute-1.amazonaws.com:5432/dfgm3fjipert9v';
+//also stream data
+pg.connect(db_url, function(err, client) {
+    if (err) {
+        console.log("Error connecting to database: " + err);
+    } else {
+        client.on('notification', function(msg) {
+            console.log("DATABASE NOTIFY: ", msg.payload);
+            // Move some data...
+        });
+        var query = client.query("LISTEN channel1");
+    }
+});
 
 module.exports = client;

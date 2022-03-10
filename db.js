2687e347eff3d7c91566c2c2ebb2fa9af4427c0b
arupsarkar-sfdc <arup.sarkar@salesforce.com>
2022-03-09 22:58:46 -0500

    Added new pg connection

--- db.js
+++ db.js
@@ -11,7 +11,7 @@ dotenv.config();
 //     ssl: true,
 // })
 
-let client = new pg.Client({
+let client = new pg ({
     user: "qvkjbbfwzcfvut",
     password: "a19763df01d1eb026c84e6d780a5b34f23d5dbecce238f53e90b41f8e0f331d8",
     database: "dfgm3fjipert9v",
